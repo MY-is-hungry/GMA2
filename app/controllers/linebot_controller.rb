c69514898d57404ee3dbf68b4cae080578f35242
MY-is-hungry <mozu113000@gmail.com>
2021-02-23 11:55:32 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -103,7 +103,7 @@ class LinebotController < ApplicationController
               user.update_attributes(arrival_lat: event.message['latitude'],arrival_lng: event.message['longitude'])
               response = open(G_URL + "origin=#{user.start_lat},#{user.start_lng}&destination=#{user.arrival_lat},#{user.arrival_lng}&key=#{G_API}")
               data = JSON.parse(response.read, {symbolize_names: true})
-              time = data[:routes][:legs][:duration]['text']
+              time = data['routes'][:legs][:duration][:text]
               
               client.reply_message(event['replyToken'], {
                 type: 'text',
