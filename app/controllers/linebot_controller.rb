160793e4e2cc4b620cf2b6e559c61b3d0bcc6a6f
MY-is-hungry <mozu113000@gmail.com>
2021-02-27 03:50:04 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -60,7 +60,7 @@ class LinebotController < ApplicationController
               });
               
             when 'ラーメン'
-              msg = message.encode('iso-2022-jp')
+              msg = message.encode('UTF-8')
               response = open(ENV['G_PLACE_URL'] + "location=#{user.start_lat},#{user.start_lng}&radius=1500&keyword=#{msg}&key=#{ENV['G_API']}")
               data = JSON.parse(response.read, {symbolize_names: true})
               client.reply_message(event['replyToken'], {
