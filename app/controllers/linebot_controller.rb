60ae012966e7163d7518a2de15539abe7989653e
MY-is-hungry <mozu113000@gmail.com>
2021-03-01 05:41:10 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -217,35 +217,25 @@ class LinebotController < ApplicationController
         return result
       when "テスト"
         result = {
-          "type": "carousel",
-          "contents": [
-            {
-              "type": "bubble",
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "First bubble"
-                  }
-                ]
-              }
-            },
-            {
-              "type": "bubble",
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Second bubble"
-                  }
-                ]
-              }
+          "type": "flex",
+          "altText": "this is a flex message",
+          "contents": {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "hello"
+                },
+                {
+                  "type": "text",
+                  "text": "world"
+                }
+              ]
             }
-          ]
+          }
         }
         return result
       end
