1f6170858137823e9cf8cd5ffde76667fcca887a
MY-is-hungry <mozu113000@gmail.com>
2021-03-01 03:50:58 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -62,7 +62,7 @@ class LinebotController < ApplicationController
             when 'ラーメン'
               url = URI.encode "#{ENV['G_PLACE_URL']}query=ラーメン&location=#{user.start_lat},#{user.start_lng}&radius=10000&radius=1500&key=#{ENV['G_API']}"
               response = open(url)
-              data = JSON.parse(response.read, {symbolize_names: true}).to_s
+              data = JSON.parse(response.read, {symbolize_names: true})[:results][0][:name]
               logger.debug(data)
               client.reply_message(event['replyToken'], {
                 type: 'text',
