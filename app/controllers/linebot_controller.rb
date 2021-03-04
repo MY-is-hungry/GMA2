380cce5c4be66164978290be5996e6cd10281e94
MY-is-hungry <mozu113000@gmail.com>
2021-03-04 09:26:16 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -69,8 +69,8 @@ class LinebotController < ApplicationController
                 url = URI.encode ENV['G_STORE_URL'] + "&query=#{hash[:results][n][:name]}&query_place_id=#{hash[:results][n][:place_id]}"
                 data[n] = {url: url, name: hash[:results][n][:name], rating: hash[:results][n][:rating], review: hash[:results][n][:user_ratings_total], address: hash[:results][n][:formatted_address]}
               end
-              logger.debug(data)
               message = change_msg(message,data)
+              logger.debug(message)
               client.reply_message(event['replyToken'], message)
             when 'テスト'
               data = change_msg(message)
