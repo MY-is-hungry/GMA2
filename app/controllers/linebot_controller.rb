3a570e1cf1fc9d8b8fd003e8d7d061e5ffce6632
MY-is-hungry <mozu113000@gmail.com>
2021-03-15 09:22:46 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -85,7 +85,8 @@ class LinebotController < ApplicationController
               fav_id = Favorite.where(user_id: commute.user_id).pluck(:place_id)
               array = Array.new
               fav_id.each do |n|
-                url = URI.encode "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{fav_id[n]}&language=ja&key=" + ENV['G_KEY']
+                # url = URI.encode "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{fav_id[n]}&language=ja&key=" + ENV['G_KEY']
+                url = URI.encode ENV['G_SEARCH_URL'] + "query=らーめん 柊&place_id=#{fav_id}&language=ja&key=" + ENV['G_KEY']
                 response = open(url)
                
                 array[n] = JSON.parse(response.read, {symbolize_names: true})
