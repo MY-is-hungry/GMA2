eaf27bb6579a95da8d34f894fd52d9fb19aea639
MY-is-hungry <mozu113000@gmail.com>
2021-03-22 05:32:30 +0000

    improve

--- app/controllers/concerns/bad_request.rb
+++ app/controllers/concerns/bad_request.rb
@@ -6,7 +6,7 @@ module BadRequest
       result = [
         {type: 'text',text: "出発地点か到着地点、もしくは両方とも設定されていません。"},
         {type: 'text',text: "「通勤設定」と送信して、設定してください。設定後、経路の中間地点を登録できます。"},
-        "quickReply": {
+        {"quickReply": {
           "items": [
             {
               "type": "action",
@@ -17,7 +17,7 @@ module BadRequest
               }
             }
           ]
-        }
+        }}
       ]
     when '通勤時間'
       result = [
