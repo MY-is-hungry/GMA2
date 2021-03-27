4344265c5d8c016bac6df6e7b10af6640220d779
MY-is-hungry <mozu113000@gmail.com>
2021-03-27 04:52:23 +0000

    improve

--- app/models/commute.rb
+++ app/models/commute.rb
@@ -4,13 +4,13 @@ class Commute < ApplicationRecord
   validates :user_id, presence: true, uniqueness: true
 
   def get_state
-    if start_lat && arrival_lat && via_place.first
+    if start_lat && end_lat && via_place.first
       state = 0 #スタート、ゴール、中間地点あり
-    elsif start_lat && arrival_lat
+    elsif start_lat && end_lat
       state = 1 #スタート、ゴールあり
     elsif start_lat
       state = 2 #スタートあり
-    elsif arrival_lat
+    elsif end_lat
       state = 3 #ゴールあり
     else
       state = 4 #通勤設定
