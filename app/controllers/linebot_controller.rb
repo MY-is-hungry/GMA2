f3eaa729d2364ff37ef0662230830e8b290e4bca
MY-is-hungry <mozu113000@gmail.com>
2021-04-12 14:15:41 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -201,6 +201,7 @@ class LinebotController < ApplicationController
               logger.debug(data[:results][0][:address_components])
               d =
                 data[:results][0][:address_components].each_with_index do |d, n|
+                  logger.debug(d[n][:type])
                   break d[n][:long_name] if d[n][:type].include?("administrative_area_level_1")
                 end
               client.reply_message(event['replyToken'], {
