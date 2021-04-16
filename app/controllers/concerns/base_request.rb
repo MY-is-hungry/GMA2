0857ac467de0b218a284f47ebb1451dadb02f15c
MY-is-hungry <mozu113000@gmail.com>
2021-04-16 06:02:49 +0000

    improve

--- app/controllers/concerns/base_request.rb
+++ app/controllers/concerns/base_request.rb
@@ -6,7 +6,7 @@ module BaseRequest
   include BasicRequest
   
   
-  def change_msg(msg, data: '', count: 0)
+  def change_msg(msg: '', data: '', count: 0)
     logger.debug(msg)
     case msg
     when 'おはよう'
