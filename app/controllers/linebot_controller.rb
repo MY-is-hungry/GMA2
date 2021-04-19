c51521bb95c17368a2831b9395063871ee393bbd
MY-is-hungry <mozu113000@gmail.com>
2021-04-19 01:41:10 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -312,7 +312,7 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::Follow
           User.create(id: event['source']['userId'])
           Commute.create(user_id: event['source']['userId'])
-          reply = change_message("follow")
+          reply = change_msg("follow")
           client.reply_message(event['replyToken'], {type: 'text', text: reply})
         when Line::Bot::Event::Unfollow
           User.find_by(id: event['source']['userId']).destroy
