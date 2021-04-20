5b6b67394c2c533ca6497c93f98f3db572f289ed
MY-is-hungry <mozu113000@gmail.com>
2021-04-20 02:05:21 +0000

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -31,6 +31,7 @@ class LinebotController < ApplicationController
             case message
             when 'おはよう'
               response = open(ENV['W_URL'] + "?zip=#{commute.start_address},jp&units=metric&lang=ja&cnt=6&APPID=" + ENV['W_KEY'])
+              logger.debug(response)
               #JSONデータをハッシュ化
               data = JSON.parse(response.read, {symbolize_names: true})
               reply = change_msg(message, data: data)
