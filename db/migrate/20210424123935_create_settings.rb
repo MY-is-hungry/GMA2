35bfdde9e036e77091802a007221a12c8180422e
MY-is-hungry <mozu113000@gmail.com>
2021-04-24 14:03:20 +0000

    improve

--- db/migrate/20210424123935_create_settings.rb
+++ db/migrate/20210424123935_create_settings.rb
@@ -7,6 +7,6 @@ class CreateSettings < ActiveRecord::Migration[5.2]
     end
     
     add_column :commutes, :setting_id, :integer
-    add_foreign_key :commutes, :setting, column: :setting_id
+    add_foreign_key :commutes, :settings, column: :setting_id
   end
 end
