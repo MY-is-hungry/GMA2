b2fe001e27a987b3ba76899e6911f4b64266a808
MY-is-hungry <mozu113000@gmail.com>
2021-04-26 13:10:58 +0000

    improve

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,27 @@ User.create(id: "Testuser")
 Setting.create(
   [
     {
-      content: 
-      {
-        "type": 'text', "text": "基本設定は完了しました。お疲れ様でした！",
-        "quickReply": {
-          "items": [
-            {
-              "type": "action",
-              "action": {
-                "type": "message",
-                "label": "次の設定へ",
-                "text": "通勤モード"
+      content:
+      [
+        {
+          type: 'text', text: "test"
+        },
+        {
+          "type": 'text', "text": "基本設定は完了しました。お疲れ様でした！",
+          "quickReply": {
+            "items": [
+              {
+                "type": "action",
+                "action": {
+                  "type": "message",
+                  "label": "次の設定へ",
+                  "text": "通勤モード"
+                }
               }
-            }
-          ]
+            ]
+          }
         }
-      }
+      ]
     },
     {
       content: ""
