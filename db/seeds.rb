152aed0ee50b50b4fcff6ae4d19f04b514658c45
MY-is-hungry <mozu113000@gmail.com>
2021-04-26 03:28:54 +0000

    improve

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,43 +4,4 @@ User.all.each do |user|
 end
 
 #app/model/commute.rb get_setting_idの順番と対応しています。
-Setting.create(
-  [
-    {
-      content: '基本設定は完了しました。お疲れ様でした！'
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    },
-    {
-      content: ''
-    }
-  ]
-)
+Setting.create(content: '基本設定は完了しました。お疲れ様でした！')
