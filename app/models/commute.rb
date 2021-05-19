a6d811de6794c8776f04b9f0f9d594ccac222d10
MY-is-hungry <mozu113000@gmail.com>
2021-05-19 09:03:04 +0000

    improve

--- app/models/commute.rb
+++ app/models/commute.rb
@@ -36,34 +36,3 @@ class Commute < ApplicationRecord
     end
   end
 end
-
-
-# 位置情報の設定状況を返す
-#   def get_state
-#     if start_lat && end_lat && via_place.first #スタート、ゴール、中間地点あり
-#       0
-#     elsif start_lat && end_lat #スタート、ゴールあり
-#       1
-#     elsif start_lat #スタートあり
-#       2
-#     elsif end_lat #ゴールあり
-#       3
-#     else #設定なし
-#       4 
-#     end
-#   end
-  
-#   #基本設定の設定状況を返す
-#   def get_setup_id
-#     if start_lat && end_lat && avoid && mode #全て設定済み
-#       1
-#     elsif start_lat && end_lat && avoid #残り通勤モード
-#       2
-#     elsif start_lat && end_lat && mode #残り経路制限
-#       3
-#     elsif start_lat && end_lat #残り通勤モード、経路制限
-#       4
-#     else #設定なし
-#       5
-#     end
-#   end
