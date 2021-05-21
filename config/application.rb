55e832cc653f96ca0e3b85851be17a7fb4d9dc2e
MY-is-hungry <mozu113000@gmail.com>
2021-05-21 06:27:43 +0000

    improve

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module GMA
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.middleware.use Rack::Attack
   end
 end
---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,5 @@
+class Rack::Attack
+  Rack::Attack.throttle('req/ip', limit: 2, period: 1.second) do |req|
+  req.ip
+end
+end
