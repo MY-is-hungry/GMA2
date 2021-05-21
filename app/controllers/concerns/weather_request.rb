cf4595f8adeb937faf96bde899e3264f9e99801d
MY-is-hungry <mozu113000@gmail.com>
2021-05-21 08:07:40 +0000

    improve

--- app/controllers/concerns/weather_request.rb
+++ app/controllers/concerns/weather_request.rb
@@ -21,6 +21,7 @@ module WeatherRequest
       weather_data[t].unshift("今日の#{city_name[t]}の天気をお知らせします。")
       t += 1
     end
+    logger.debug(weather_data.count)
     if weather_data.count == 2
       result = [{type: "text", text: weather_data[0].join}, {type: "text", text: weather_data[1].join}]
     else
