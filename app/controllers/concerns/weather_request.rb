6b9b56ea02e0622e7cb773d45af0a6706554e552
MY-is-hungry <mozu113000@gmail.com>
2021-05-30 13:53:04 +0000

    improve

--- app/controllers/concerns/weather_request.rb
+++ app/controllers/concerns/weather_request.rb
@@ -17,7 +17,7 @@ module WeatherRequest
         temp = l[:main][:temp].round(1) #温度
         weather = get_weather(l[:weather][0][:id]) #天気idをもとにメソッドから天気を取得
         result[n].push("\n\n#{time}時 天気:雨  温度:#{temp}℃")
-        rain = true if weather.in?['雷雨', '激しい雨', '雨', 'にわか雨']
+        rain = true if weather.in?(['雷雨', '激しい雨', '雨', 'にわか雨'])
       end
       result[n].unshift("今日の#{city_name[n]}の天気をお知らせします。")
     end
